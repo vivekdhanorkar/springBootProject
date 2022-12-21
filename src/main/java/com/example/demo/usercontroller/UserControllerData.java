b03a67b8ca1e463a17f867ddc192fb63f28c732e
Vivek <vpdhanora@gmail.com>
2022-12-21 20:23:25 +0530

    custom controller

---
+++ src/main/java/com/example/demo/usercontroller/UserControllerData.java
@@ -0,0 +1,16 @@
+package com.example.demo.usercontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewUserController
+{
+
+    public void newMethod(){
+
+        System.out.println("new method created");
+
+    }
+
+
+}
